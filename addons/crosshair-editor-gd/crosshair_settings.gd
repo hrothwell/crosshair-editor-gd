77bb95b846b4af87ed6b063d22f301fe838196d0
Haiden <46227616+hrothwell@users.noreply.github.com>
2026-09-16 23:10:46 -0700

    fix issue serializing data (#3)

--- addons/crosshair-editor-gd/crosshair_settings.gd
+++ addons/crosshair-editor-gd/crosshair_settings.gd
@@ -9,7 +9,7 @@ class_name CrosshairSettings extends Resource
 @abstract func to_dict() -> Dictionary
 
 static func to_json(setting: CrosshairSettings) -> String:
-	return JSON.stringify(JSON.from_native(setting))
+	return JSON.stringify(JSON.from_native(setting.to_dict()))
 
 static func from_json(json: String) -> CrosshairSettings:
 	var result: CrosshairSettings = null
